f046fb68c2aeeaf14e5792176a8a17c46af86e6b
Joe W <whobbes22@gmail.com>
2023-04-12 15:30:45 -0700

    updated tests, added 4th test

--- _tests_/haiku.test.js
+++ _tests_/haiku.test.js
@@ -2,9 +2,9 @@ import Haiku from "../src/haiku";
 
 describe('Haiku', () =>{
 
+
   test("should verify if there are 3 seperate strings", () => {
     const haiku = new Haiku("hi","yo","hello");
-
     expect(haiku.line1).toEqual("hi");
     expect(haiku.line2).toEqual("yo");
     expect(haiku.line3).toEqual("hello");
@@ -19,12 +19,23 @@ describe('Haiku', () =>{
   });
 
   test("should count all vowels in a string", () => {
-    const haiku = new Haiku("hi","yo","hello");
+    const haiku = new Haiku("hi","yo","hellos");
+    let vowelArray = [];
+    haiku.splitter();
     haiku.vowelCounter();
-    expect(haiku.syllableArray[0]).toEqual(1);
-    expect(haiku.syllableArray[1]).toEqual(1);
-    expect(haiku.syllableArray[2]).toEqual(2);
+    expect(haiku.vowelArray[0]).toEqual(1);
+    expect(haiku.vowelArray[1]).toEqual(1);
+    expect(haiku.vowelArray[2]).toEqual(2);
   });
 
-
+  test("should verify the number of syllables in words that end with 'e'", () => {
+    const haiku = new Haiku("pie","abide", "fresh");
+    haiku.splitter();
+    haiku.vowelCounter();
+    expect(haiku.vowelArray[0]).toEqual(1);
+    expect(haiku.vowelArray[1]).toEqual(2);
+    expect(haiku.vowelArray[2]).toEqual(1);
+  }); 
 });
+
+ 
