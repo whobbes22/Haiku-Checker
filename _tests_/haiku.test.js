1ca4383e408d953968019851a18423af9a710940
Joe W <whobbes22@gmail.com>
2023-04-12 10:35:37 -0700

    add second test

--- _tests_/haiku.test.js
+++ _tests_/haiku.test.js
@@ -8,6 +8,15 @@ describe('Haiku', () =>{
     expect(haiku.line1).toEqual("hi");
     expect(haiku.line2).toEqual("yo");
     expect(haiku.line3).toEqual("hello");
-  
   });
+
+  test("Should split strings into an array each index containing 1 letter", () => {
+    const haiku = new Haiku("hi","yo","hello");
+    haiku.splitter();
+    expect(haiku.line1).toEqual(["h","i"]);
+    expect(haiku.line2).toEqual(["y","o"]);
+    expect(haiku.line3).toEqual(["h","e","l","l","o"]);
+  });
+
+  test("should verify the number of vowels in a given string")
 });
