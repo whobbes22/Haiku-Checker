fd6e28949d0ad5af2bb0c1a7aaa687bfe7dfb3da
Joe W <whobbes22@gmail.com>
2023-04-12 10:04:30 -0700

    add first test

---
+++ _tests_/haiku.test.js
@@ -0,0 +1,13 @@
+//import 
+
+describe('check', () =>{
+
+  test("should verify if there are 3 seperate strings", () => {
+    const haiku = new Haiku("hi","yo","hello");
+
+    expect(haiku.line1).toEqual("hi");
+    expect(haiku.line2).toEqual("yo");
+    expect(haiku.line3).toEqual("hello");
+  
+  });
+});
